f9674f6e064477332b87995b9840939561e3bf5c
Subhra Swarup Behera <subhra.behera@tavant.com>
2017-04-25 15:25:46 +0530

    router

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+
+
 import { IProduct } from './product';
 
 @Component({
     templateUrl:'app/products/product-detail.component.html'
 })
 export class ProductDetailComponent{
-    pageTitle:string='Produc Detail';
+    pageTitle:string='Product Detail';
     product: IProduct;
+
+    constructor(private _route:ActivatedRoute,private _router:Router){
+
+    }
+   
+
+    ngOnInit(): void{
+        let id= +this._route.snapshot.params['id'];
+        this.pageTitle += `:${id}`;
+    }
+    onBack():void{
+        this._router.navigate(['/products']);
+    }
 }
